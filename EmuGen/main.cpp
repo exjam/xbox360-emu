4acac27f183e2f66ea006b6c43dd594707c4a0e0
James <james.benton2@gmail.com>
2013-10-19 11:04:17 +0100

    Removed some test code from emugen
    Was using the file to quickly test some bit stuff and accidently
    committed it :)

--- EmuGen/main.cpp
+++ EmuGen/main.cpp
@@ -1,39 +1,8 @@
 #include "emugen.h"
 #include <iostream>
 
-#include "../Common/util/bits.h"
-
-template<typename SrcType, int Start, int Size>
-struct BitWrapper
-{
-   BitWrapper(SrcType &value) : value(value)
-   {
-   }
-
-   operator SrcType()
-   {
-      return (value >> Start) & bits::mask<SrcType>(Size);
-   }
-
-   BitWrapper &operator=(const SrcType &v)
-   {
-      auto mask = bits::mask<SrcType>(Size);
-      value &= ~(mask << Start);
-      value |= (v & mask) << Start;
-      return *this;
-   }
-
-   SrcType& value;
-};
-
 int main(int argc, char **argv)
 {
-   int bitField = 0;
-
-   auto bw = BitWrapper<int, 31, 1>(bitField);
-   bw = 0x1;
-   int val = bw;
-
    if (argc < 3) {
       std::cout << "Usage: " << argv[0] << " <definition file> <output directory>" << std::endl;
       return 0;
---
+++ PowerPC/emugen_instr_disasm.cpp
@@ -0,0 +1,455 @@
+/* add */
+bool add(State *state, Instruction instr)
+{
+}
+
+/* addc */
+bool addc(State *state, Instruction instr)
+{
+}
+
+/* adde */
+bool adde(State *state, Instruction instr)
+{
+}
+
+/* addi */
+bool addi(State *state, Instruction instr)
+{
+}
+
+/* addic */
+bool addic(State *state, Instruction instr)
+{
+}
+
+/* addic. */
+bool addico(State *state, Instruction instr)
+{
+}
+
+/* addis */
+bool addis(State *state, Instruction instr)
+{
+}
+
+/* addme */
+bool addme(State *state, Instruction instr)
+{
+}
+
+/* addze */
+bool addze(State *state, Instruction instr)
+{
+}
+
+/* and */
+bool and(State *state, Instruction instr)
+{
+}
+
+/* andc */
+bool andc(State *state, Instruction instr)
+{
+}
+
+/* andi. */
+bool andio(State *state, Instruction instr)
+{
+}
+
+/* andis. */
+bool andiso(State *state, Instruction instr)
+{
+}
+
+/* b */
+bool b(State *state, Instruction instr)
+{
+}
+
+/* bc */
+bool bc(State *state, Instruction instr)
+{
+}
+
+/* bcctr */
+bool bcctr(State *state, Instruction instr)
+{
+}
+
+/* bclr */
+bool bclr(State *state, Instruction instr)
+{
+}
+
+/* cmp */
+bool cmp(State *state, Instruction instr)
+{
+}
+
+/* cmpi */
+bool cmpi(State *state, Instruction instr)
+{
+}
+
+/* cmpl */
+bool cmpl(State *state, Instruction instr)
+{
+}
+
+/* cmpli */
+bool cmpli(State *state, Instruction instr)
+{
+}
+
+/* cntlzd */
+bool cntlzd(State *state, Instruction instr)
+{
+}
+
+/* cntlzw */
+bool cntlzw(State *state, Instruction instr)
+{
+}
+
+/* crand */
+bool crand(State *state, Instruction instr)
+{
+}
+
+/* crandc */
+bool crandc(State *state, Instruction instr)
+{
+}
+
+/* creqv */
+bool creqv(State *state, Instruction instr)
+{
+}
+
+/* crnand */
+bool crnand(State *state, Instruction instr)
+{
+}
+
+/* crnor */
+bool crnor(State *state, Instruction instr)
+{
+}
+
+/* cror */
+bool cror(State *state, Instruction instr)
+{
+}
+
+/* crorc */
+bool crorc(State *state, Instruction instr)
+{
+}
+
+/* crxor */
+bool crxor(State *state, Instruction instr)
+{
+}
+
+/* dcbf */
+bool dcbf(State *state, Instruction instr)
+{
+}
+
+/* dcbst */
+bool dcbst(State *state, Instruction instr)
+{
+}
+
+/* dcbt */
+bool dcbt(State *state, Instruction instr)
+{
+}
+
+/* dcbtst */
+bool dcbtst(State *state, Instruction instr)
+{
+}
+
+/* dcbz */
+bool dcbz(State *state, Instruction instr)
+{
+}
+
+/* divd */
+bool divd(State *state, Instruction instr)
+{
+}
+
+/* divdu */
+bool divdu(State *state, Instruction instr)
+{
+}
+
+/* divw */
+bool divw(State *state, Instruction instr)
+{
+}
+
+/* divwu */
+bool divwu(State *state, Instruction instr)
+{
+}
+
+/* eciwx */
+bool eciwx(State *state, Instruction instr)
+{
+}
+
+/* ecowx */
+bool ecowx(State *state, Instruction instr)
+{
+}
+
+/* eieio */
+bool eieio(State *state, Instruction instr)
+{
+}
+
+/* eqv */
+bool eqv(State *state, Instruction instr)
+{
+}
+
+/* extsb */
+bool extsb(State *state, Instruction instr)
+{
+}
+
+/* extsh */
+bool extsh(State *state, Instruction instr)
+{
+}
+
+/* extsw */
+bool extsw(State *state, Instruction instr)
+{
+}
+
+/* fabs */
+bool fabs(State *state, Instruction instr)
+{
+}
+
+/* fadd */
+bool fadd(State *state, Instruction instr)
+{
+}
+
+/* fadds */
+bool fadds(State *state, Instruction instr)
+{
+}
+
+/* fcfid */
+bool fcfid(State *state, Instruction instr)
+{
+}
+
+/* fcmpo */
+bool fcmpo(State *state, Instruction instr)
+{
+}
+
+/* fcmpu */
+bool fcmpu(State *state, Instruction instr)
+{
+}
+
+/* fctid */
+bool fctid(State *state, Instruction instr)
+{
+}
+
+/* fctidz */
+bool fctidz(State *state, Instruction instr)
+{
+}
+
+/* fctiw */
+bool fctiw(State *state, Instruction instr)
+{
+}
+
+/* fctiwz */
+bool fctiwz(State *state, Instruction instr)
+{
+}
+
+/* fdiv */
+bool fdiv(State *state, Instruction instr)
+{
+}
+
+/* fdivs */
+bool fdivs(State *state, Instruction instr)
+{
+}
+
+/* fmadd */
+bool fmadd(State *state, Instruction instr)
+{
+}
+
+/* fmadds */
+bool fmadds(State *state, Instruction instr)
+{
+}
+
+/* fmr */
+bool fmr(State *state, Instruction instr)
+{
+}
+
+/* fmsub */
+bool fmsub(State *state, Instruction instr)
+{
+}
+
+/* fmsubs */
+bool fmsubs(State *state, Instruction instr)
+{
+}
+
+/* fmul */
+bool fmul(State *state, Instruction instr)
+{
+}
+
+/* fmuls */
+bool fmuls(State *state, Instruction instr)
+{
+}
+
+/* fnabs */
+bool fnabs(State *state, Instruction instr)
+{
+}
+
+/* fneg */
+bool fneg(State *state, Instruction instr)
+{
+}
+
+/* fnmadd */
+bool fnmadd(State *state, Instruction instr)
+{
+}
+
+/* fnmadds */
+bool fnmadds(State *state, Instruction instr)
+{
+}
+
+/* fnmsub */
+bool fnmsub(State *state, Instruction instr)
+{
+}
+
+/* fnmsubs */
+bool fnmsubs(State *state, Instruction instr)
+{
+}
+
+/* fres */
+bool fres(State *state, Instruction instr)
+{
+}
+
+/* frsp */
+bool frsp(State *state, Instruction instr)
+{
+}
+
+/* frsqrte */
+bool frsqrte(State *state, Instruction instr)
+{
+}
+
+/* fsel */
+bool fsel(State *state, Instruction instr)
+{
+}
+
+/* fsqrt */
+bool fsqrt(State *state, Instruction instr)
+{
+}
+
+/* fsqrts */
+bool fsqrts(State *state, Instruction instr)
+{
+}
+
+/* fsub */
+bool fsub(State *state, Instruction instr)
+{
+}
+
+/* fsubs */
+bool fsubs(State *state, Instruction instr)
+{
+}
+
+/* icbi */
+bool icbi(State *state, Instruction instr)
+{
+}
+
+/* isync */
+bool isync(State *state, Instruction instr)
+{
+}
+
+/* mfspr */
+bool mfspr(State *state, Instruction instr)
+{
+}
+
+/* stb */
+bool stb(State *state, Instruction instr)
+{
+}
+
+/* stbu */
+bool stbu(State *state, Instruction instr)
+{
+}
+
+/* std */
+bool std(State *state, Instruction instr)
+{
+}
+
+/* stdu */
+bool stdu(State *state, Instruction instr)
+{
+}
+
+/* sth */
+bool sth(State *state, Instruction instr)
+{
+}
+
+/* sthu */
+bool sthu(State *state, Instruction instr)
+{
+}
+
+/* stw */
+bool stw(State *state, Instruction instr)
+{
+}
+
+/* stwu */
+bool stwu(State *state, Instruction instr)
+{
+}
+
